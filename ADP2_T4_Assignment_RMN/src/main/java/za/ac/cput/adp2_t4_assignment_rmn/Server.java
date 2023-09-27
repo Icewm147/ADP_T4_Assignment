f14683725fa80dba932edef88dc9a78626642a76
Mogammad Mas'ood Lamera <128643400+mmlamera@users.noreply.github.com>
2023-09-27 17:26:07 +0200

    Update Server.java

--- ADP2_T4_Assignment_RMN/src/main/java/za/ac/cput/adp2_t4_assignment_rmn/Server.java
+++ ADP2_T4_Assignment_RMN/src/main/java/za/ac/cput/adp2_t4_assignment_rmn/Server.java
@@ -1,13 +1,68 @@
-
 package za.ac.cput.adp2_t4_assignment_rmn;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 /**
  *
- * @authors
- * -Nicholas van der Nest (222749180)
- * -Raeesah Khan (219)
- * -Mogammad Mas'ood Lamera (221376321)
+ * @authors -Nicholas van der Nest (222749180) -Raeesah Khan (219) -Mogammad
+ * Mas'ood Lamera (221376321)
  */
 public class Server {
-    //add server code
+
+    private static ObjectOutputStream out;
+    private static ObjectInputStream in;
+    private static ServerSocket serverSocket;
+    private static Socket clientSocket;
+    private static Object receivedObject;
+
+    public Server() {
+        try {
+            serverSocket = new ServerSocket(12345);
+            System.out.println("Server is listening....");
+
+            clientSocket = serverSocket.accept();
+
+        } catch (IOException ex) {
+            System.out.println("Server cannot listen. Error: " + ex.getMessage());
+        }
+    }
+
+    public void getStreams() {
+        try {
+            out = new ObjectOutputStream(clientSocket.getOutputStream());
+            out.flush();
+            in = new ObjectInputStream(clientSocket.getInputStream());
+        } catch (IOException ex) {
+            System.out.println("Cannot get Streams " + ex.getMessage());
+        }
+    }
+    
+    
+    
+     private static void closeConnection() {
+        try {
+            out.writeObject("Server has closed");
+            out.flush();
+            out.close();
+            in.close();
+            clientSocket.close();
+            System.out.println("Server has closed");
+            System.exit(0);
+
+        } catch (IOException ex) {
+            System.out.println("Server cannot close. Error " + ex.getMessage());
+
+        }
+    }
+      public static void main(String[] args) {
+        WorkerStudent srs = new WorkerStudent();
+        srs.getStreams();
+        srs.processClient();
+        WorkerCourse wc = new WorkerCourse();
+    }
+
 }
