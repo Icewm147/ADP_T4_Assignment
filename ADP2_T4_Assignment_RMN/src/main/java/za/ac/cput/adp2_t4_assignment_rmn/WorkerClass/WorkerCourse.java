6b952b823f496ce6fd841b9d56c5f5bda1b7f8f1
Icewm147 <127603940+Icewm147@users.noreply.github.com>
2023-10-01 05:35:53 -0400

    sum1

---
+++ ADP2_T4_Assignment_RMN/src/main/java/za/ac/cput/adp2_t4_assignment_rmn/WorkerClass/WorkerCourse.java
@@ -0,0 +1,33 @@
+package za.ac.cput.adp2_t4_assignment_rmn.WorkerCourse;
+
+/**
+ *
+ * @author nicho
+ */
+public class WorkerCourse {
+    private String courseName;
+    private String CourseCode;
+
+    public WorkerCourse(String courseName, String CourseCode) {
+        this.courseName = courseName;
+        this.CourseCode = CourseCode;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public String getCourseCode() {
+        return CourseCode;
+    }
+
+    public void setCourseCode(String CourseCode) {
+        this.CourseCode = CourseCode;
+    }
+
+
+}
