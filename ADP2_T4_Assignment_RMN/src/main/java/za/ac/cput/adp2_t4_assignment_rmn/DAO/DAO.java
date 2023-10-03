94ff4b94e5158694572c32868d32e216afb94b7c
Icewm147 <127603940+Icewm147@users.noreply.github.com>
2023-10-03 08:42:49 -0400

    Update DAO.java

--- ADP2_T4_Assignment_RMN/src/main/java/za/ac/cput/adp2_t4_assignment_rmn/DAO/DAO.java
+++ ADP2_T4_Assignment_RMN/src/main/java/za/ac/cput/adp2_t4_assignment_rmn/DAO/DAO.java
@@ -10,4 +10,36 @@ package za.ac.cput.adp2_t4_assignment_rmn.DAO;
  */
 public class DAO {
     
+    public Connection connectToDB(){
+	Connection conn = null;
+	try {
+		String url = ""; // add DB url
+		String username = ""; // add DB username
+		String password = ""; // add DB password
+
+		conn = DriverManager.getConnection(url, username, password);
+		if(conn != null){
+			System.out.println("Connected to DB");
+		} catch(SQLException ex) {
+			ex.printStackTrace();
+                        System.out.println("Failed to connect to the database.");
+		}
+	} return conn;
+    }
+
+    public boolean addStudentToDB(WorkerStudent student){
+    	boolean isAdded = false;
+	String query = "INSERT INTO Students (studentNumber, name, course)"; // enter correct details to match DB & getters and setters
+	try{
+	PreparedStatement statement = dbConnection.prepareStatement(query);
+		ps.setInt(student.getStudentNumber());
+		ps.setString(student.getName());
+		ps.setString(student.getCourse());
+
+		isAdded = ps.executeUpdate() == 1; // Execute the query. If it returns 1, the insertion was successful
+	}catch(SQLException e){
+		e.printStackTrace();
+	}
+	return isAdded;
+    }
 }
