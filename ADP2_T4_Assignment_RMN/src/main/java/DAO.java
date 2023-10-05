cd7ec5dd2aefb280a8ca0dba5ae843754e880f9e
Icewm147 <127603940+Icewm147@users.noreply.github.com>
2023-10-05 15:10:09 -0400

    DAO Commit Oct5th (again)

--- ADP2_T4_Assignment_RMN/src/main/java/DAO.java
+++ ADP2_T4_Assignment_RMN/src/main/java/DAO.java
@@ -13,7 +13,6 @@ public class DAO {
 //    public DAO(){
 //        connectToDB(); <---------------------------CHECK IF IT'S NEEDED
 //    }
-    
     public Connection connectToDB() {
         Connection con = null;
         try {
@@ -45,10 +44,10 @@ public class DAO {
 //                socket.close();
 //            }
     }
-    
-    public void deleteStudent(int studentNumber) throws SQLException{
+
+    public void deleteStudent(int studentNumber) throws SQLException {
         String query = "DELETE FROM Students WHERE Stud_Number = ?";
-        
+
         PreparedStatement statement = connectToDB().prepareStatement(query);
         statement.setInt(1, studentNumber);
         statement.executeUpdate();
@@ -56,68 +55,86 @@ public class DAO {
 
     public void deleteCourseFromDB(int courseID) throws SQLException {
         String query = "DELETE FROM Course WHERE CourseID = ?";
-        
-            PreparedStatement statement = connectToDB().prepareStatement(query);
-            statement.setInt(1, courseID);
-            statement.executeUpdate();
+
+        PreparedStatement statement = connectToDB().prepareStatement(query);
+        statement.setInt(1, courseID);
+        statement.executeUpdate();
     }
 
     public void addCourseToDB(WorkerCourse course) throws IOException, SQLException {
         String query = "INSERT INTO COURSE (COURSE_CODE, COURSE_DESCRIPTION) VALUES (?, ?)";
-            PreparedStatement statement = connectToDB().prepareStatement(query);
-            //statement.setString(1, courseCode);
-            //statement.setString(2, courseName);                  //<<------------I think this will come from the GUI?
-            //statement.setString(3, courseDescription);
-            statement.executeUpdate();
+        PreparedStatement statement = connectToDB().prepareStatement(query);
+        //statement.setString(1, courseCode);
+        //statement.setString(2, courseName);                  //<<------------I think this will come from the GUI?
+        //statement.setString(3, courseDescription);
+        statement.executeUpdate();
     }
 
     public List<WorkerStudent> getAllStudents() throws SQLException {
         List<WorkerStudent> students = new ArrayList<>();
         String query = "SELECT * FROM Students";
-        
-            PreparedStatement statement = connectToDB().prepareStatement(query);
-            ResultSet result = statement.executeQuery();
 
-            while (result.next()) {
-                String studentNumber = result.getString("Student_Number");   //<<------------ENTER CORRECT INFO FROM DB AND GETTERS AND SETTERS FOR STUDENTS
-                String name = result.getString("Name");
-                String course = result.getString("Course");
+        PreparedStatement statement = connectToDB().prepareStatement(query);
+        ResultSet result = statement.executeQuery();
 
-                //WorkerStudent student = new WorkerStudent(studentNumber, name, course); <------Delete after testing if not needed
-                students.add(new WorkerStudent(studentNumber, name, course));
-            }
+        while (result.next()) {
+            String studentNumber = result.getString("Student_Number");   //<<------------ENTER CORRECT INFO FROM DB AND GETTERS AND SETTERS FOR STUDENTS
+            String name = result.getString("Name");
+            String course = result.getString("Course");
+
+            //WorkerStudent student = new WorkerStudent(studentNumber, name, course); <------Delete after testing if not needed
+            students.add(new WorkerStudent(studentNumber, name, course));
+        }
         return students;
     }
 
     public List<WorkerCourse> getAllCourses() throws SQLException {
         List<WorkerCourse> courses = new ArrayList<>();
         String query = "SELECT * FROM Course";
-            PreparedStatement statement = connectToDB().prepareStatement(query);
-            ResultSet result = statement.executeQuery();
+        PreparedStatement statement = connectToDB().prepareStatement(query);
+        ResultSet result = statement.executeQuery();
 
-            while (result.next()) {
-                String courseID = result.getString("COURSE_CODE");     //<<------------ENTER CORRECT INFO FROM DB AND GETTERS AND SETTERS FOR COURSE((THIS OE SHOULD BE RIGHT))
-                String courseDescription = result.getString("COURSE_DESCRIPTION");
+        while (result.next()) {
+            String courseID = result.getString("COURSE_CODE");     //<<------------ENTER CORRECT INFO FROM DB AND GETTERS AND SETTERS FOR COURSE((THIS OE SHOULD BE RIGHT))
+            String courseDescription = result.getString("COURSE_DESCRIPTION");
 
-                //WorkerCourse course = new WorkerCourse(courseID, courseDescription); <------Delete after testing if not needed
-                courses.add(new WorkerCourse(courseID, courseDescription));
-            }
+            //WorkerCourse course = new WorkerCourse(courseID, courseDescription); <------Delete after testing if not needed
+            courses.add(new WorkerCourse(courseID, courseDescription));
+        }
         return courses;
     }
-    
+
     public List<WorkerStudent> studentsPerCourse(int courseID) throws SQLException {
         String query = "SELECT student.* FROM student JOIN student_course ON student.id = student_course.student_id WHERE student_course.course_id = ?"; //<--------- query will need fixing with right table names
-        
+
         List<WorkerStudent> studentsOfCourse = new ArrayList<>();
-        
+
+        PreparedStatement statement = connectToDB().prepareStatement(query);
+        statement.setInt(1, courseID);
+        ResultSet result = statement.executeQuery();
+        while (result.next()) {
+            String studName = result.getString("courseID");
+            String studLastName = result.getString("courseName");
+            String studNum = result.getString("studNum");
+            studentsOfCourse.add(new WorkerStudent(studName, studLastName, studNum));
+        }
         return studentsOfCourse;
     }
-    
+
     public List<WorkerCourse> coursesPerStudent(int studentNumber) throws SQLException {
         String query = "SELECT course.* FROM course JOIN student_coourse ON course.id = student_course.course_id WHERE student_course.student_id = ?"; //<---- same as query@studentsPerCourse()
-        
+
         List<WorkerCourse> coursesOfStudent = new ArrayList<>();
-        
+
+        PreparedStatement statement = connectToDB().prepareStatement(query);
+        statement.setInt(1, studentNumber);
+        ResultSet result = statement.executeQuery();
+        while (result.next()) {
+            String courseID = result.getString("courseID");
+            String courseName = result.getString("courseName");
+            coursesOfStudent.add(new WorkerCourse(courseID, courseName));
+        }
+
         return coursesOfStudent;
     }
 }
