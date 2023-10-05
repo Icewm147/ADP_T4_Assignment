40029ee0cebb63ed820e7d034723e3c3cc740f75
Icewm147 <127603940+Icewm147@users.noreply.github.com>
2023-10-05 03:47:25 -0400

    DAO Commit Oct5th

--- ADP2_T4_Assignment_RMN/src/main/java/DAO.java
+++ ADP2_T4_Assignment_RMN/src/main/java/DAO.java
@@ -1,17 +1,12 @@
-<<<<<<< HEAD
 
-//
-//import java.awt.List;
-
-=======
 import java.util.List;
->>>>>>> ecb9c79b96eaa3ff3ff114a8f75df7dd934ca3b8
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
+
 /**
  *
  * @author 222749180
@@ -32,93 +27,98 @@ public class DAO {
         return con;
     }
 
-    public boolean addStudentToDB(WorkerStudent student){
-    	boolean studentIsAdded = false;
-	String query = "INSERT INTO Students (studentNumber, name, course)"; // enter correct details to match DB & getters and setters
-	try{
-	PreparedStatement statement = connectToDB().prepareStatement(query);
-		statement.setString(1, student.getStudentNumber());
-		statement.setString(2, student.getStudentName());
-		statement.setString(3, student.getStudentLastName());
+    public boolean addStudentToDB(WorkerStudent student) {
+        boolean studentIsAdded = false;
+        String query = "INSERT INTO Students (studentNumber, name, course)"; // enter correct details to match DB & getters and setters
+        try {
+            PreparedStatement statement = connectToDB().prepareStatement(query);
+            statement.setString(1, student.getStudentNumber());
+            statement.setString(2, student.getStudentName());
+            statement.setString(3, student.getStudentLastName());
 
-		studentIsAdded = statement.executeUpdate() == 1; // Execute the query. If it returns 1, the insertion was successful
-	}catch(SQLException e){
-		e.printStackTrace();
-	}
-	return studentIsAdded;
+            studentIsAdded = statement.executeUpdate() == 1; // Execute the query. If it returns 1, the insertion was successful
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return studentIsAdded;
     }
 
-    public boolean deleteCourseFromDB(int courseID){
-    	boolean courseDeleted = false;
+    public boolean deleteCourseFromDB(int courseID) {
+        boolean courseDeleted = false;
         String query = "DELETE FROM Course WHERE CourseID = ?";
 
-	try {
-	  PreparedStatement statement = connectToDB().prepareStatement(query);
-	  statement.setInt(1, courseID);
+        try {
+            PreparedStatement statement = connectToDB().prepareStatement(query);
+            statement.setInt(1, courseID);
 
-	  courseDeleted = statement.executeUpdate() == 1;
-	} catch (SQLException e) {
-	  e.printStackTrace();
-	}
+            courseDeleted = statement.executeUpdate() == 1;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         return courseDeleted;
     }
 
-    public boolean addCourseToDB(){
-    	boolean courseIsAdded = false;
-	String query = "INSERT INTO COURSE (COURSE_CODE, COURSE_DESCRIPTION) VALUES (?, ?)";
-	try{
-		PreparedStatement statement = connectToDB().prepareStatement(query);
-		//statement.setString(1, courseCode);
-		//statement.setString(2, courseName);                  //<<------------I think this will come from the GUI?
-		//statement.setString(3, courseDescription);
+    public boolean addCourseToDB() {
+        boolean courseIsAdded = false;
+        String query = "INSERT INTO COURSE (COURSE_CODE, COURSE_DESCRIPTION) VALUES (?, ?)";
+        try {
+            PreparedStatement statement = connectToDB().prepareStatement(query);
+            //statement.setString(1, courseCode);
+            //statement.setString(2, courseName);                  //<<------------I think this will come from the GUI?
+            //statement.setString(3, courseDescription);
 
-		courseIsAdded = statement.executeUpdate() == 1;
-	} catch (SQLException e) {
-		e.printStackTrace();
-	}
-	return courseIsAdded;
+            courseIsAdded = statement.executeUpdate() == 1;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return courseIsAdded;
     }
 
     public List<WorkerStudent> getAllStudents() {
-    List<WorkerStudent> students = new ArrayList<>();
-    String query = "SELECT * FROM Students";
-    try {
-        Statement statement = connectToDB().createStatement();
-        ResultSet result = statement.executeQuery(query);
-        
-        while (result.next()) {
-            String studentNumber = result.getString("Student_Number");   //<<------------ENTER CORRECT INFO FROM DB AND GETTERS AND SETTERS FOR STUDENTS
-            String name = result.getString("Name");
-            String course = result.getString("Course");
-            
-            WorkerStudent student = new WorkerStudent(studentNumber, name, course);
-            students.add(student);
+        List<WorkerStudent> students = new ArrayList<>();
+        String query = "SELECT * FROM Students";
+        try {
+            Statement statement = connectToDB().createStatement();
+            ResultSet result = statement.executeQuery(query);
+
+            while (result.next()) {
+                String studentNumber = result.getString("Student_Number");   //<<------------ENTER CORRECT INFO FROM DB AND GETTERS AND SETTERS FOR STUDENTS
+                String name = result.getString("Name");
+                String course = result.getString("Course");
+
+                WorkerStudent student = new WorkerStudent(studentNumber, name, course);
+                students.add(student);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
-        
-    } catch (SQLException e) {
-        e.printStackTrace();
-    }
-    return students;
+        return students;
     }
 
     public List<WorkerCourse> getAllCourses() {
-    List<WorkerCourse> courses = new ArrayList<>();
-    String query = "SELECT * FROM Course";
-    try {
-        Statement statement = connectToDB().createStatement();
-        ResultSet result = statement.executeQuery(query);
-        
-        while (result.next()) {
-            String courseID = result.getString("COURSE_CODE");     //<<------------ENTER CORRECT INFO FROM DB AND GETTERS AND SETTERS FOR COURSE((THIS OE SHOULD BE RIGHT))
-            String courseDescription = result.getString("COURSE_DESCRIPTION");
-            
-            WorkerCourse course = new WorkerCourse(courseID, courseDescription);
-            courses.add(course);
+        List<WorkerCourse> courses = new ArrayList<>();
+        String query = "SELECT * FROM Course";
+        try {
+            Statement statement = connectToDB().createStatement();
+            ResultSet result = statement.executeQuery(query);
+
+            while (result.next()) {
+                String courseID = result.getString("COURSE_CODE");     //<<------------ENTER CORRECT INFO FROM DB AND GETTERS AND SETTERS FOR COURSE((THIS OE SHOULD BE RIGHT))
+                String courseDescription = result.getString("COURSE_DESCRIPTION");
+
+                WorkerCourse course = new WorkerCourse(courseID, courseDescription);
+                courses.add(course);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
-        
-    } catch (SQLException e) {
-        e.printStackTrace();
+        return courses;
     }
-    return courses;
-}
+    
+    //Delete Student Method
+    //Enroll student into course Method
+    //Add student to course
+    //CRUD operations that are missing
 }
